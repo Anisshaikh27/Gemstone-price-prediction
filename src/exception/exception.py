368b6479f37c112fe057358cc20723cfc29e299b
Anisshaikh27 <anisshaikh2708@gmail.com>
2025-07-09 15:21:23 +0530

    resolved error as warnings for flake and mypy in ci.yml file

--- src/exception/exception.py
+++ src/exception/exception.py
@@ -1,25 +1,33 @@
 import sys
 
+import traceback
 
-class customexception(Exception):
+# Alternative approach using traceback module (more robust)
+class CustomException(Exception):
+    """Custom exception class using traceback module."""
+    
+    def __init__(self, error_message: str) -> None:
+        self.error_message = error_message
+        
+        # Get traceback information
+        tb = traceback.extract_tb(sys.exc_info()[2])
+        if tb:
+            # Get the last frame (where the error occurred)
+            last_frame = tb[-1]
+            self.file_name = last_frame.filename
+            self.lineno = last_frame.lineno
+        else:
+            self.file_name = "unknown"
+            self.lineno = 0
+    
+    def __str__(self) -> str:
+        return "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+            self.file_name, self.lineno, str(self.error_message)
+        )
 
-    def __init__(self,error_message,error_details:sys):
-        self.error_message=error_message
-        _,_,exc_tb=error_details.exc_info()
-        print(exc_tb)
 
-        self.lineno=exc_tb.tb_lineno
-        self.file_name=exc_tb.tb_frame.f_code.co_filename
-
-    def __str__(self):
-        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        self.file_name, self.lineno, str(self.error_message))
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     try:
-        a=1/0
-
+        a = 1 / 0
     except Exception as e:
-        #print(e)
-        raise customexception(e,sys)
+        raise CustomException(str(e))
