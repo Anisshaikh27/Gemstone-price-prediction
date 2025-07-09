fc9d938d69d016ba506c0fd6cdc3e58514039d99
Anisshaikh27 <anisshaikh2708@gmail.com>
2025-07-09 16:41:14 +0530

    Fixed customexception class and cross-platform path handling

--- src/exception/exception.py
+++ src/exception/exception.py
@@ -1,32 +1,17 @@
 import sys
 import traceback
 
-# Alternative approach using traceback module (more robust)
 class customexception(Exception):
-    """Custom exception class using traceback module."""
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
     
-    def __init__(self, error_message: str) -> None:
-        self.error_message = error_message
-        
-        # Get traceback information
-        tb = traceback.extract_tb(sys.exc_info()[2])
-        if tb:
-            # Get the last frame (where the error occurred)
-            last_frame = tb[-1]
-            self.file_name = last_frame.filename
-            self.lineno = last_frame.lineno
-        else:
-            self.file_name = "unknown"
-            self.lineno = 0
-    
-    def __str__(self) -> str:
-        return "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
-            self.file_name, self.lineno, str(self.error_message)
-        )
+    def __str__(self):
+        return self.error_message
 
-
-if __name__ == "__main__":
-    try:
-        a = 1 / 0
-    except Exception as e:
-        raise customexception(str(e))
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = f"Error occurred in python script name [{file_name}] line number [{line_number}] error message [{str(error)}]"
+    return error_message
